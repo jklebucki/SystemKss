48cda587e30123f8e95685b3d5366cf23966b3c8
Jarosław Kłębucki <j.klebucki@ajksoftware.pl>
2025-04-07 20:10:08 +0200

    Update assembly information and add .gitignore file

--- tests/SystemKss.Application.Tests/obj/Debug/net8.0/SystemKss.Application.Tests.AssemblyInfo.cs
+++ tests/SystemKss.Application.Tests/obj/Debug/net8.0/SystemKss.Application.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SystemKss.Application.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82c5e726b7f5460a2237262a1854a5c22eb13faa")]
 [assembly: System.Reflection.AssemblyProductAttribute("SystemKss.Application.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SystemKss.Application.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
